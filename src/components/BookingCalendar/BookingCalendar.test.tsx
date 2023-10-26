06b1f87a5c7fde52f1de3db2b12ba9fed785aa12
stellamartinscrp <stella@edujob.comls>
2023-10-26 19:27:12 -0300

    fix axios unit test error

--- src/components/BookingCalendar/BookingCalendar.test.tsx
+++ src/components/BookingCalendar/BookingCalendar.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import BookingCalendar from './BookingCalendar.component';
-import { BrowserRouter } from 'react-router-dom';
 
 describe('BookingCalendar', () => {
   const dateRange = {
@@ -14,46 +13,37 @@ describe('BookingCalendar', () => {
 
   it('renders the component with provided props', () => {
     const { getByText, getByTestId } = render(
-      <BrowserRouter>
-        <BookingCalendar
-          dateRange={dateRange}
-          setDateRange={setDateRange}
-          pastMonth={pastMonth}
-          bike={bike}
-        />
-      </BrowserRouter>
+      <BookingCalendar dateRange={dateRange} setDateRange={setDateRange} pastMonth={pastMonth} bike={bike} />
     );
 
     expect(getByText('Select date and time')).toBeInTheDocument();
-
+    
     expect(getByTestId('bike-select')).toBeInTheDocument();
   });
-
+  
   it('displays mobile calendar when the button is clicked', () => {
+
     const originalInnerWidth = window.innerWidth;
+  
 
     window.innerWidth = 320;
-
+  
     const { getByText, getByTestId } = render(
-      <BrowserRouter>
-        <BookingCalendar
-          dateRange={dateRange}
-          setDateRange={setDateRange}
-          pastMonth={pastMonth}
-          bike={bike}
-        />
-      </BrowserRouter>
+      <BookingCalendar dateRange={dateRange} setDateRange={setDateRange} pastMonth={pastMonth} bike={bike} />
     );
-
+  
     expect(getByText('Select date and time')).toBeInTheDocument();
-    expect(setDateRange).not.toHaveBeenCalled();
+    expect(setDateRange).not.toHaveBeenCalled(); 
+  
 
     const mobileDateButton = getByTestId('mobile-date-button');
     fireEvent.click(mobileDateButton);
-
+  
     expect(getByText('Select date and time')).toBeInTheDocument();
     expect(setDateRange).not.toHaveBeenCalled();
-
+  
     window.innerWidth = originalInnerWidth;
   });
+
+ 
 });
